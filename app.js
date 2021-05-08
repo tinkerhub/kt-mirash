20eb863ce605bc201d52a06926b6b6805fc750c6
Abid Aboobaker <abid@Abids-MacBook-Air.local>
2021-05-08 13:55:50 +0530

    Converting userid to UserID

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { setUserRole } = require("./airtable/setUserRole");
 const { newMembers } = require('./discord/newMemeber');
 
 const { wrongId } = require("./config/var");
-const { errorMsg} = require("./discord/erorMsg");
+const { errorMsg } = require("./discord/erorMsg");
 
 // ? config values
 const guildID = config.get('guildID');
@@ -23,7 +23,7 @@ client.on("ready", () => {
     console.log("I am ready!");
 });
 
-client.on('message', async (message) => {
+client.on('message', async(message) => {
 
 
     if (message.content.toLowerCase().startsWith("r") &&
@@ -34,9 +34,8 @@ client.on('message', async (message) => {
 
         await newMembers(myGuild, base, message, client);
 
-    } else if (!message.author.bot && message.channel.id === `744627218743033887`) { 
-       await errorMsg(message, wrongId('user'));
+    } else if (!message.author.bot && message.channel.id === `744627218743033887`) {
+        await errorMsg(message, wrongId('user'));
     }
 
 })
-
