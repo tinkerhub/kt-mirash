295f10d4b0abdf72fa5c03e995b8362bc8c7160d
sreehari2003 <sreeharivijaya2003@gmail.com>
2023-04-25 23:50:47 +0530

    feat: added member intent

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
 	],
 	partials: [Partials.Channel],
 });
